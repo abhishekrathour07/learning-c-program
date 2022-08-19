b9d81694d2103bbab27d3dad023e2de3681d94b4
Abhishek Rathour <singhrathourabhishek7@gmail.com>
2022-08-19 21:12:22 +0530

    adding changes

--- 01_practice1.c
+++ 01_practice1.c
@@ -2,7 +2,7 @@
 
 
 int main(){
-    // Changes Here 1
+    // Changes Here 1 2
     
     int length = 4, bredth = 8;
     printf("area if rectangle is %d", length * bredth);
--- 01_practice2.c
+++ 01_practice2.c
@@ -4,8 +4,8 @@ int main()
     // Changes here tooo
     int r = 4;
     float pie = 3.14;
-   int h=3;
-    printf("area of the circle is %f\n", pie*r*r);
-    printf("Area of the cylinder is %f", pie*r*r*h);
+    int h = 3;
+    printf("area of the circle is %f\n", pie * r * r);
+    printf("Area of the cylinder is %f", pie * r * r * h);
     return 0;
 }
