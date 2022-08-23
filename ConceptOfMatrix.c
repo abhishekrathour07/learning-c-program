78ef25e997a1df9a30a3039ecaf3fc678109e7a6
Abhishek Rathour <singhrathourabhishek7@gmail.com>
2022-08-23 11:01:15 +0530

    concept of matrix

---
+++ ConceptOfMatrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][4], i, j, sum = 0;
+    printf("enter the elements of array\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("matrix of array is\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d\t", arr[i][j]);
+            sum = sum + arr[i][j];
+    }
+          printf("\n");
+        }
+    printf("\nsum of the array is %d\n", sum);
+    return 0;
+}
---
+++ MuntiplyOfTwoMatrix.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void main()
+{
+    int a1[2][2], a2[2][2], sum[2][2], i, j;
+    printf(" Enter the Elements in a1 are:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &a1[i][j]);
+        }
+    }
+    printf(" Entter the Elements in a2 are:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &a2[i][j]);
+        }
+    }
+    printf("Element in a1 are:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d\t", a1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("element in a2 are:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d\t", a2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("multiplicatiom of a1 and a2 matrix are :\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            sum[i][j] = a1[i][j] * a2[i][j];
+        }
+    }
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+}
