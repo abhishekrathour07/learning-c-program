c1250fa421289244570007f5d88c32e18d71a820
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-10-12 01:34:55 +0530

    correction

--- 04_practice6.c
+++ 04_practice6.c
@@ -1,26 +1,22 @@
 // write a program to find prime number as input taken by users
 #include <stdio.h>
 
-void main()
+int main()
 {
-    int num, i, prime, min;
-    printf("enter the no. to find prime \n");
+    int num, i, min = 0, prime = 0;
+    printf("Enter the number to check prime:");
     scanf("%d", &num);
     min = num / 2;
     for (i = 2; i <= min; i++)
     {
         if (num % i == 0)
         {
-            prime = 0;
+            printf("Number is not prime");
+            prime = 1;
             break;
         }
     }
     if (prime == 0)
-    {
-        printf("this is not a prime number");
-    }
-    else
-    {
-        printf("this is a prime number");
-    }
+        printf("Number is prime");
+    return 0;
 }
