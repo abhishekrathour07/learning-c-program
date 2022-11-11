f060966799c5e7d0b1cd4f2341accee967dcdad0
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-11-11 23:45:36 +0530

    quad

---
+++ Quadratic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+    float a,b,c,disc,r1,r2,sqrt;
+    printf("Enter three co-efficient");
+    scanf("%f %f %f",&a,&b,&c);
+    if(a==0){
+        printf("****Not a quadratic equation****");
+    }
+    else{
+        b*b -4.0 *2*c;
+    if(disc ==0){
+    printf("complex Root\n");
+    disc = disc;
+    r1 = -b(2.0*a)
+    r2 =sqrt(disc);
+    }
+}
+}
