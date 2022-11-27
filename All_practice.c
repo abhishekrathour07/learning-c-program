176e2f65701ac8872a5e4f930e6e81d45e098a25
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-11-28 02:14:55 +0530

    fact sort

--- All_practice.c
+++ All_practice.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-int main(){
-    int t1=0,t2=1,t3=t1+t2;
-    int n ,i;
-    printf("Enter the num to find fibo series");
-    scanf("%d",&n);
-    printf("FIBO SERIES ARe %d ,%d,",t1,t2);
-    for(i=2;i<n;i++){
-        printf("%d,",t3);
-        t1=t2;
-        t2=t3;t3 =t1+t2;
+int fact(int n)
+{
+    if(n!=0){
+        return n* fact(n-1);
+    }else{
+        return 1;
     }
 }
+    int main(){
+        int num; 
+        printf("enter the num");
+        scanf("%d",&num);
+        printf("fact of %d is %d",num,fact(num));
+    }
