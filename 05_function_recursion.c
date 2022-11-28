f5d3839f8bdc81b9f0b46ee26587f5f9f90c635b
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-11-28 23:37:46 +0530

    new files

--- 05_function_recursion.c
+++ 05_function_recursion.c
@@ -22,3 +22,17 @@ int main()
     printf(" The factorial of %d is %d ", a, factorial(a));
     return 0;
 }
+// int fact(int n)
+// {
+//     if(n!=0){
+//         return n* fact(n-1);
+//     }else{
+//         return 1;
+//     }
+// }
+//     int main(){
+//         int num; 
+//         printf("enter the num");
+//         scanf("%d",&num);
+//         printf("fact of %d is %d",num,fact(num));
+//     }
--- All_practice.c
+++ All_practice.c
@@ -1,15 +1,1 @@
 #include <stdio.h>
-int fact(int n)
-{
-    if(n!=0){
-        return n* fact(n-1);
-    }else{
-        return 1;
-    }
-}
-    int main(){
-        int num; 
-        printf("enter the num");
-        scanf("%d",&num);
-        printf("fact of %d is %d",num,fact(num));
-    }
