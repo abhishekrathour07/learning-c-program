cbe95030c1c8745b3480dcd0dbe5632e1fc6fb42
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-01-06 02:07:02 +0530

    insertion sort

---
+++ 12_7insertionSort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main(){
+    int i,j,n,temp,a[25];
+    printf("Enter the size of array:\n");
+    scanf("%d",&n);
+    printf("Enter the unsorted array:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        temp =a[i];
+        j=i-1;
+        while(j>=0 && a[j]>temp){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1] =temp;
+    }
+    printf("Insertion sort are :\n");
+    for(i=0;i<n;i++){
+     printf("%3d",a[i]);
+    }
+}
