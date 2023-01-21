1338895bffbd5a1c3ca0d1b6f607084b9eb6e364
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-01-21 10:58:17 +0530

    no Changes

--- All_practice.c
+++ All_practice.c
@@ -1,16 +1,11 @@
-for (i = 0; i < (n - 1); i++) {
-      position = i;
-      for (j = i + 1; j < n; j++) {
-         if (arr[position] > arr[j])
-            position = j;
-      }
-      if (position != i) {
-         swap = arr[i];
-         arr[i] = arr[position];
-         arr[position] = swap;
-      }
-   }
-   for (i = 0; i < n; i++)
-      printf("%d\t", arr[i]);
-   return 0;
+#include<stdio.h>
+void main(){
+   int l,b;
+   printf("the value of L is %d",l);
+scanf("%d",&l);
+   printf("the value of B is %d",b);
+scanf("%d",&b);
+printf("the value of l*b is is %d",l*b);
+
+
 }
