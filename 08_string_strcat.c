ff807af5839113772ba1ca1569416a7c2a104c75
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-01-25 20:25:48 +0530

    soRT

--- 08_string_strcat.c
+++ 08_string_strcat.c
@@ -4,8 +4,8 @@ void main()
 {
     char string1[43]= "Boliye ";
     // char *string2 = "jai shree";
-    strcat(string1, "ka boli mardwa"); // -->adding value on string
-    strcat(string1, ", Ram ji ki jai bola ho");
+    strcat(string1, "jai shree"); // -->adding value on string
+    strcat(string1, ", Ram");
     printf(" string1 = %s\n",string1);
   
 
--- 12_7insertionSort.c
+++ 12_7insertionSort.c
@@ -1,24 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main(){
-    int i,j,n,temp,a[25];
+void main()
+{
+    int i, j, n, temp, a[25];
     printf("Enter the size of array:\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter the unsorted array:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
-    for(i=0;i<n;i++){
-        temp =a[i];
-        j=i-1;
-        while(j>=0 && a[j]>temp){
-            a[j+1]=a[j];
+    for (i = 1; i < n; i++)
+    {
+        temp = a[i];
+        j = i - 1;
+        while (j >= 0 && a[j] > temp)
+        {
+            a[j + 1] = a[j];
             j--;
         }
-        a[j+1] =temp;
+        a[j + 1] = temp;
     }
     printf("Insertion sort are :\n");
-    for(i=0;i<n;i++){
-     printf("%3d",a[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("%3d", a[i]);
     }
 }
