338529c47349692b68f178610162fab070e5bb51
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-01-27 23:46:37 +0530

    new

--- All_practice.c
+++ All_practice.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
-void main(){
-   int l,b;
-   printf("the value of L is %d",l);
-scanf("%d",&l);
-   printf("the value of B is %d",b);
-scanf("%d",&b);
-printf("the value of l*b is is %d",l*b);
 
-
+int add(int a,int b ,int c)
+{
+   int d=a+b-c;
+   return d;
+}
+int add(int a,int b,int c)
+{
+   int d=a+b+c;
+   return d;
+}
+int main()
+{
+   int a=2,b=4,c=5;
+   add(a,b,c);
+   add(a,b,c);
+   printf("%d",add);
+   return 0;
 }
